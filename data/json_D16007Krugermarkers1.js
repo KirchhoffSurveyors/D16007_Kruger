de3cda27efc08f07678ab497080fa673229c838f
David Barkhuisen <dave@kirchhoffsurveyors.co.za>
2016-04-13 16:47:31 +0200

    Added files via upload

---
+++ data/json_D16007Krugermarkers1.js
@@ -0,0 +1,13 @@
+var json_D16007Krugermarkers1={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "id": 1.000000, "Descr.": "1,330 km from dam" }, "geometry": { "type": "Point", "coordinates": [ 31.26607, -25.006544 ] } },
+{ "type": "Feature", "properties": { "id": 2.000000, "Descr.": "580 m NNW from view point loop" }, "geometry": { "type": "Point", "coordinates": [ 31.299924, -24.984335 ] } },
+{ "type": "Feature", "properties": { "id": 3.000000, "Descr.": "40 m past the track on the right hand side if facing North" }, "geometry": { "type": "Point", "coordinates": [ 31.339623, -24.975325 ] } },
+{ "type": "Feature", "properties": { "id": 4.000000, "Descr.": "680 m before the track on the right hand side if facing North" }, "geometry": { "type": "Point", "coordinates": [ 31.380264, -24.973995 ] } },
+{ "type": "Feature", "properties": { "id": 5.000000, "Descr.": "800 m past the track on the right hand side if facing North" }, "geometry": { "type": "Point", "coordinates": [ 31.420972, -24.975862 ] } },
+{ "type": "Feature", "properties": { "id": 6.000000, "Descr.": "250 m past the track on the right hand side if facing North" }, "geometry": { "type": "Point", "coordinates": [ 31.437116, -24.988739 ] } }
+]
+}
